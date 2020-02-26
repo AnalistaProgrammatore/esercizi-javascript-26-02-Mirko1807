fb2ff4b54c3b34a63571e72319cc8b64c5850aad
Mirko1807 <cavolamirko@gmail.com>
2020-02-26 20:43:11 +0100

    range

--- range.js
+++ range.js
@@ -1,4 +1,3 @@
-
 const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step)).reduce((a, x) => a + x);
 const range2 = ({ start, stop, step }) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step)).reduce((a, x) => a + x);
 let start = 1; let stop = 10; let step = 2;
