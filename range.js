a47e5069e4f9e67fecd853067181678a1d36fc4d
Mirko1807 <cavolamirko@gmail.com>
2020-02-26 20:42:06 +0100

    range

--- range.js
+++ range.js
@@ -1,3 +1,7 @@
-/**
-* Scrivere qui il codice dell'esercizio 1 del capitolo 4 di eloquentJS
-**/
+
+const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step)).reduce((a, x) => a + x);
+const range2 = ({ start, stop, step }) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step)).reduce((a, x) => a + x);
+let start = 1; let stop = 10; let step = 2;
+let v1 = range(start, stop, step);
+let v2 = range2({ stop, start, step });
+console.log(v1, v2);
