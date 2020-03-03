06d55d4f8af9377b61bb8ee234a992bbe9196c60
Mirko1807 <59826817+Mirko1807@users.noreply.github.com>
2020-03-03 14:37:49 +0100

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -1,1 +1,12 @@
+const reduceBase = (v, callback, a) => {
+    let b = callback(v, a)
+    return b
+}
+const reducer = (v, a) => {
+    for (let i = 0; i < v.length; i++) a += v[i]
 
+    return a
+}
+let v = [1, 2, 3]
+let b = reduceBase(v, reducer, 2)
+console.log(b)
