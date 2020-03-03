15bbeec987dd2d0b5ea091a9d132d9e02a7046b8
Mirko1807 <59826817+Mirko1807@users.noreply.github.com>
2020-03-03 14:25:25 +0100

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,1 +1,17 @@
-
+const filterBase = (v, callback) => {
+    callback(v)
+}
+const filterReduce = (v, callback) => {
+    v.reduce(callback)
+}
+const reducer = (a, x) => {
+    if (a < 5) console.log(a)
+    if (x < 5) console.log(x)
+}
+let condition = (v) => {
+    for (let i = 0; i < v.length; i++)
+        if (v[i] > 5) console.log(v[i])
+}
+V = [1, 2, 3, 4, 5, 6]
+filterBase(V, condition)
+filterReduce(V, reducer)
