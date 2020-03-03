563153fdfdb253b3b0e5a7e430cd706a5ce41601
Mirko1807 <59826817+Mirko1807@users.noreply.github.com>
2020-03-03 14:25:56 +0100

    Update some.js

--- some.js
+++ some.js
@@ -1,1 +1,25 @@
+const someBase = (v, callback) => {
+    let b = callback(v)
+    return b
+}
+const someReduce = (v, callback) => {
+    return v.reduce(callback)
+}
+const reducer = (a, x) => {
+
+    if (a < 5) return true
+    if (x < 5) return true
+    return false
+}
+let condition = (v) => {
+    for (let i = 0; i < v.length; i++)
+        if (v[i] > 5) return true
+    return false
+}
+V = [6, 2, 3, 4, 5, 6]
+let b = someBase(V, condition)
+let d = someReduce(V, reducer)
+console.log(b, d)
+
+
 
